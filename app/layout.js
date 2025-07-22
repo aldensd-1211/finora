79369ea1a0e76f04d4216882d8a334c1a6c43cd9
Dinesh <dineshjakharofiicial@gmail.com>
2025-07-22 18:49:01 +0530

    Setup user authentication with Clerk

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,21 +12,23 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={`${inter.className}`}>
-        <Header />
+    <ClerkProvider fallbackRedirectUrl="/">
+      <html lang="en">
+        <body className={`${inter.className}`}>
+          <Header />
 
-        <main className="min-h-screen">{children}</main>
+          <main className="min-h-screen">{children}</main>
 
-        <footer className="bg-blue-50 py-12">
-          <div className="container mx-auto px-4 text-center text-gray-600">
-            <p>
-              © 2025 Finora — Built by Alden Dcunha & Dinesh Jakhar. All rights
-              reserved.
-            </p>
-          </div>
-        </footer>
-      </body>
-    </html>
+          <footer className="bg-blue-50 py-12">
+            <div className="container mx-auto px-4 text-center text-gray-600">
+              <p>
+                © 2025 Finora — Built by Alden Dcunha & Dinesh Jakhar. All
+                rights reserved.
+              </p>
+            </div>
+          </footer>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
